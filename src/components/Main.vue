e3dabeb060f3345cfe5a050a645f63b4c1f2d9b9
Mitchell Johnson <orangutandevelopment@gmail.com>
2017-09-16 17:52:37 -0400

    Initial Work

---
+++ src/components/Main.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="main">
+    <div class="content_wrapper">
+      <h2><icon name="bolt" scale="1.2"></icon> {{ first_header_title }}</h2>
+      <div class="pure-g">
+        <div class="pure-u-1-3">
+          <videoitem v-for="item in items" v-bind:key="item.message"></videoitem>
+        </div>
+        <div class="pure-u-1-3">
+          <videoitem v-for="item in items" v-bind:key="item.message"></videoitem>
+        </div>
+        <div class="pure-u-1-3">
+          <videoitem v-for="item in items" v-bind:key="item.message"></videoitem>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'vue-awesome/icons'
+import VideoItem from '@/components/VideoItem'
+import Icon from 'vue-awesome/components/Icon'
+
+export default {
+  name: 'main',
+  data () {
+    return {
+      first_header_title: 'Your Feed',
+      second_header_title: 'Trending in Canada',
+      third_header_title: 'Trending Worldwide',
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' }
+      ]
+    }
+  },
+  components: {
+    'videoitem': VideoItem,
+    'icon': Icon
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.pure-g {
+  margin: 0 -12px;
+}
+</style>
---
+++ src/components/VideoItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="videoitem">
+    <img src="../assets/thumbnails/1.jpg" class="logo">
+    <div class="vid_info">
+        <h2>{{ video_title }}</h2>
+        <p>{{ video_description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'videoitem',
+  data () {
+    return {
+      msg: '1',
+      video_title: "Vine about Boats",
+      video_description: "You'll never see this coming..."
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.videoitem {
+    margin: 0;
+    min-height: 84px;
+    display: inline-block;
+    padding: 12px 26px 12px 12px;
+    cursor: pointer;
+} .videoitem:hover {
+    background-color: rgba(255, 255, 255, 0.02);
+}
+
+h2 {
+    margin: 6px 0 0;
+    font-size: 18px;
+    display: inline-block;
+}
+
+p {
+    padding: 5px 1px;
+    font-size: 14px;
+    color: #a2a2a2;
+    margin: 0;
+}
+
+img {
+    max-height: 84px;
+    float: left;
+}
+
+.vid_info {
+    display: inline-block;
+    padding-left: 12px;
+}
+
+</style>
